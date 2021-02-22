939088acf7b0ab3df5970457838dc98c497308b0
IrisTc <1010939908@qq.com>
2021-02-22 14:44:27 +0800

    keep-result改为手动根据target的大小情况分割成三种大小文件

--- keep-result.sh
+++ keep-result.sh
@@ -7,6 +7,7 @@ tensorcount=$(ps -ef | grep python3 | grep -v grep | wc -l)
 cd /mnt/d/spider/zwData
 if [ $tensorcount == 0 ]
 then
+	echo "scrapy is restart"
 	files=$(ls $target_path)
 	for filename in $files
 	do
@@ -16,9 +17,11 @@ then
 			break
 		fi
 	done
-	echo "scrapy restart"
+	# result_filename=$result_type
+	result_filename=${filename::-7}
 	# result_filename=${filename%.*}".csv"
-	python3 -m scrapy crawl $result_type -o "$result_path$result_type.csv"
+	echo $result_filename
+	python3 -m scrapy crawl $result_type -o "$result_path$result_filename.csv" -s LOG_FILE=warn.log
 else
 	echo "scrapy is running"
 fi
